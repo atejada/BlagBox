b5fdfe053d6f3983d816ccebb3c5aa7a64c15e0d
Blag <atejada@gmail.com>
2024-09-26 08:49:25 -0500

    Fix typo
    It's "Sent from" not "Send from" 🤦🏻‍♂️

--- BlagBox_Client.py
+++ BlagBox_Client.py
@@ -466,7 +466,7 @@ class ReplyScreen(Screen):
         pass
         message = nylas.messages.find(os.environ.get("BLAGBOX_GRANT_ID"), messageid[0]).data
         self.query_one("#body").text = "<br>====<br>" + get_message(self, messageid[0])
-        self.query_one("#body").text += "<br><br>Send from The ultimate Email, Calendar and Contacts Terminal Client" 
+        self.query_one("#body").text += "<br><br>Sent from The ultimate Email, Calendar and Contacts Terminal Client" 
         self.query_one("#email_from").value = message.from_[0]['email']
         self.query_one("#title").value = message.subject #"Re: " + message.subject
 
@@ -544,7 +544,7 @@ class ComposeEmail(Screen):
         yield Input(placeholder="Title", id="title")
         body = TextArea(id="body")
         body.show_line_numbers = False
-        body.text = "<br><br>Send from my Terminal Email Client"
+        body.text = "<br><br>Sent from my Terminal Email Client"
         yield body
         yield Horizontal(
             Button("Send!", variant="primary", id="send"),
